a03f317419379528c32ad8d1378075ce4ef3cd19
alpoliakov <alpoliakov73@gmail.com>
2021-03-20 18:31:26 +0200

    feat: Added Layout.

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,14 +4,20 @@ import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import React from 'react';
 
+import Layout from '../components/Layout';
 import { useApollo } from '../lib/apollo';
+import { AuthProvider } from '../lib/useAuth';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
