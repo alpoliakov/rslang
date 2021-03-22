a7a7efbb3adf5d99bc4c77918cfe350fe0d05dc5
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-22 13:10:47 +0100

    feat: add icons, basic elements

--- app/components/MiniGames/Sprint/Sprint.tsx
+++ app/components/MiniGames/Sprint/Sprint.tsx
@@ -1,53 +1,103 @@
 import React, { useState } from 'react';
-import { CloseButton } from '@chakra-ui/react';
 import { Timer } from './Timer';
-import { Button, ButtonGroup } from '@chakra-ui/react';
-import { ArrowForwardIcon, ArrowBackIcon, CheckCircleIcon, NotAllowedIcon } from '@chakra-ui/icons';
+import { Button, ButtonGroup, IconButton } from '@chakra-ui/react';
+import {
+  ArrowForwardIcon,
+  ArrowBackIcon,
+  CheckCircleIcon,
+  NotAllowedIcon,
+  BellIcon,
+  CloseIcon,
+  WarningIcon,
+} from '@chakra-ui/icons';
 import { ModalQuit } from '../ModalQuit/ModalQuit';
+import { RiMusic2Fill } from 'react-icons/ri';
 
 const Sprint = () => {
-  const [showGame, setShowGame] = useState(true);
+  const [isSoundOn, setSound] = useState(true);
+  const [quitGame, setQuitGame] = useState(false);
+  const [counter, setCounter] = useState(0);
 
-  const onCloseGame = () => setShowGame(false);
+  const handleAnswer = () => {
+    setCounter(counter + 10);
+  };
 
+  const onSwitchSound = () => {
+    setSound(!isSoundOn);
+  };
+
+  const onQuitGame = () => {
+    setQuitGame(true);
+  };
   return (
     <>
       <div className="sprint-container">
         <Timer />
         <div className="sprint-board-outer">
-          <div className="sprint-icons">
-            <CheckCircleIcon />
-            <CheckCircleIcon />
-            <NotAllowedIcon />
+          <div className="sprint-board-head">
+            <div key="counter" className="sprint-counter">
+              {counter ? `+ ${counter}` : counter}
+            </div>
+            <IconButton
+              className="sprint-music"
+              variant="ghost"
+              size="lg"
+              colorScheme={isSoundOn ? 'whiteAlpha' : 'red'}
+              aria-label="Switch sound"
+              onClick={onSwitchSound}
+              icon={<RiMusic2Fill />}
+            />
           </div>
-          <div className="sprint-pics">
-            <img src="https://img.icons8.com/fluent/50/000000/osprey.png" />
-            <img src="https://img.icons8.com/fluent/50/000000/falcon.png" />
-            <img src="https://img.icons8.com/fluent/50/000000/peacock.png" />
-          </div>
-          <div className="sprint-english">daily</div>
-          <div className="sprint-translation">высокомерный</div>
-          <div className="sprint-buttons">
-            <ButtonGroup size="lg" spacing="12">
-              <Button colorScheme="red">Неверно</Button>
-              <Button colorScheme="green" className="green-button">
-                Верно
-              </Button>
-            </ButtonGroup>
-          </div>
-          <div className="sprint-arrows">
-            <ArrowBackIcon />
-            <ArrowForwardIcon />
+
+          <div className="sprint-board-inner">
+            <div className="sprint-icons">
+              <div className="sprint-result-icons">
+                <CheckCircleIcon />
+                <CheckCircleIcon />
+                <NotAllowedIcon />
+              </div>
+              <IconButton
+                className="sprint-sound"
+                variant="ghost"
+                colorScheme={isSoundOn ? 'green' : 'red'}
+                aria-label="Switch sound"
+                onClick={onSwitchSound}
+                icon={<BellIcon />}
+              />
+            </div>
+            <div className="sprint-pics">
+              <img src="https://img.icons8.com/fluent/50/000000/osprey.png" />
+              <img src="https://img.icons8.com/fluent/50/000000/falcon.png" />
+              <img src="https://img.icons8.com/fluent/50/000000/peacock.png" />
+            </div>
+            <div className="sprint-english">daily</div>
+            <div className="sprint-translation">высокомерный</div>
+            <div className="sprint-buttons">
+              <ButtonGroup size="lg" spacing="12">
+                <Button colorScheme="red" onClick={handleAnswer}>
+                  Неверно
+                </Button>
+                <Button colorScheme="green" className="green-button" onClick={handleAnswer}>
+                  Верно
+                </Button>
+              </ButtonGroup>
+            </div>
+            <div className="sprint-arrows">
+              <ArrowBackIcon />
+              <ArrowForwardIcon />
+            </div>
           </div>
         </div>
-        <div className="sprint-close">
-          <Button colorScheme="whiteAlpha" variant="ghost">
-            X
-          </Button>
-          {/* <CloseButton size="lg" onClick={onCloseGame} colorScheme="whiteAlpha" /> */}
-        </div>
+        <IconButton
+          className="sprint-close"
+          colorScheme="whiteAlpha"
+          aria-label="Close game"
+          variant="ghost"
+          onClick={onQuitGame}
+          icon={<CloseIcon />}
+        />
       </div>
-      <ModalQuit />
+      {quitGame && <ModalQuit setQuitGame={setQuitGame} quitGame={quitGame} />}
     </>
   );
 };
