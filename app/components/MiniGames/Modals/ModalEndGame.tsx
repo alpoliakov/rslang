90bf06c30483142a17030c65cda3f65fc17a56f0
Yasya Mashnenko <ya.mashnenko@gmail.com>
2021-03-24 14:56:30 +0300

    feat: add exit buttons to modal

--- app/components/MiniGames/Modals/ModalEndGame.tsx
+++ app/components/MiniGames/Modals/ModalEndGame.tsx
@@ -1,7 +1,23 @@
 import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+import { useHotkeys } from 'react-hotkeys-hook';
 
-const ModalEndGame = () => {
+const ModalEndGame = ({ timeOver, setTimeOver }) => {
+  const [showMod, setModal] = useState(true);
+  useHotkeys('esc', () => setTimeOver(!timeOver));
+  useHotkeys('enter', () => setTimeOver(!timeOver));
+
+  const handleClickClose = () => {
+    setModal(!showMod);
+    setTimeOver(!timeOver);
+  };
+
+  const handleClickReturn = () => {
+    setModal(!showMod);
+    setTimeOver(!timeOver);
+  };
+
   return (
     <div className="modal-container">
       <div className="modal">
@@ -11,6 +27,16 @@ const ModalEndGame = () => {
             <CircularProgressLabel size="40px">60</CircularProgressLabel>
           </CircularProgress>
           <br /> баллов!
+          <div className="actions">
+            <ButtonGroup size="md" spacing="12">
+              <Button colorScheme="green" className="green-button" onClick={handleClickClose}>
+                Да
+              </Button>
+              <Button colorScheme="red" onClick={handleClickReturn}>
+                Нет
+              </Button>
+            </ButtonGroup>
+          </div>
         </div>
       </div>
     </div>
