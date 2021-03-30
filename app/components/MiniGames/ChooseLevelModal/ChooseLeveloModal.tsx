f00a0dd80810b2ba1e35de3cb27ee4d30d846d62
Yasya Mashnenko <ya.mashnenko@gmail.com>
2021-03-31 00:49:36 +0300

    fix: fix values

--- app/components/MiniGames/ChooseLevelModal/ChooseLeveloModal.tsx
+++ app/components/MiniGames/ChooseLevelModal/ChooseLeveloModal.tsx
@@ -1,18 +1,17 @@
 import { Select } from '@chakra-ui/react';
 import React from 'react';
 
-const ChooseLevelModal = ({ level, setLevel }) => {
-  const handleChange = (e) => setLevel(e.target.value);
-  console.log(level);
+const ChooseLevelModal = ({ group, setGroup }) => {
+  const handleChange = (e) => setGroup(e.target.value);
   return (
     <div className="chooseModal-select">
-      <Select value={level} placeholder="Выбери уровень сложности" onChange={handleChange}>
+      <Select value={group} placeholder="Выбери уровень сложности" onChange={handleChange}>
         <option value="option1">1 уровень</option>
         <option value="option2">2 уровень</option>
         <option value="option3">3 уровень</option>
-        <option value="option1">4 уровень</option>
-        <option value="option2">5 уровень</option>
-        <option value="option3">6 уровень</option>
+        <option value="option4">4 уровень</option>
+        <option value="option5">5 уровень</option>
+        <option value="option6">6 уровень</option>
       </Select>
     </div>
   );
