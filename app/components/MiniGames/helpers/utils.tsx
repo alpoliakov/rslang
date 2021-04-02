80ba4c977a214aaac37748efe75cedd9fc19cac6
Yasya Mashnenko <ya.mashnenko@gmail.com>
2021-04-02 23:02:34 +0300

    refactor: move fetchWords to other fetchWords

--- app/components/MiniGames/helpers/utils.tsx
+++ app/components/MiniGames/helpers/utils.tsx
@@ -1,5 +1,3 @@
-import { initializeApollo } from '../../../lib/apollo';
-import { WordsDocument } from '../../../lib/graphql/words.graphql';
 import { dino, dinoEgg, dragon } from './constants';
 
 // ------- SPRINT -------
@@ -65,56 +63,12 @@ export const checkAnswerSavanna = (wordToCheck, answer) => {
   return wordToCheck.word === answer.word;
 };
 
-export const fetchCurrentWords = async (group, page, setLoading, setWords, setCurrentPage) => {
-  console.log('inside fetchCurrentWords', group, group === '', typeof group);
-  if (group === '') return;
-  console.log('here fetchCurrentWords');
-  const apollo = initializeApollo();
-  setLoading(true);
-  const { data } = await apollo.query({
-    query: WordsDocument,
-    variables: { group: Number(group), page },
-  });
-  console.log('here fetchCurrentWords');
-
-  const words = [...data.words];
-  setWords([...data.words]);
-  console.log(words);
-  if (words) {
-    setLoading(false);
-  }
-  if (setCurrentPage) {
-    setCurrentPage(page + 1);
-  }
-  // console.log('const words', words[0].word, words[0].wordTranslate);
-  // return words;
-};
-
 // ------- NEWGAME -------
 export const checkAnswerNewGame = (wordToCheck, answer) => {
   return wordToCheck.word === answer.toLowerCase().trim();
 };
 
 // ------- AUDIOCALL -------
-
-export const fetchCurrentWordsAudiocall = async (group, page, setLoading, setWords) => {
-  const apollo = initializeApollo();
-  setLoading(true);
-  const { data } = await apollo.query({
-    query: WordsDocument,
-    variables: { group, page },
-  });
-
-  const words = [...data.words];
-  setWords(words.splice(9, 10));
-  console.log(words);
-  if (words) {
-    setLoading(false);
-  }
-  // setCurrentPage(page);
-  // console.log('const words', words[0].word, words[0].wordTranslate);
-  // return words;
-};
 
 export const getNextWordAudiocall = (arr, learnedWords) => {
   const [mainWord] = arr
