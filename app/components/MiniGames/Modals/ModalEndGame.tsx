8dba362a7b33547ceb3b0275c0c7689a4b196635
Yasya Mashnenko <ya.mashnenko@gmail.com>
2021-04-03 17:26:42 +0300

    feat: add accordion for extended results

--- app/components/MiniGames/Modals/ModalEndGame.tsx
+++ app/components/MiniGames/Modals/ModalEndGame.tsx
@@ -2,22 +2,19 @@ import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from '@chakra-ui/react';
 import { useHotkeys } from 'react-hotkeys-hook';
 
 const ModalEndGame = ({ counter }) => {
-  // const [showMod, setModal] = useState(true);
   // useHotkeys('esc', () => setTimeOver(!timeOver));
   // useHotkeys('enter', () => setTimeOver(!timeOver));
-
-  // const handleClickClose = () => {
-  //   setModal(!showMod);
-  //   setTimeOver(!timeOver);
-  // };
-
-  // const handleClickReturn = () => {
-  //   setModal(!showMod);
-  //   setTimeOver(!timeOver);
-  // };
 
   return (
     <div className="modal-container">
@@ -28,11 +25,27 @@ const ModalEndGame = ({ counter }) => {
             <CircularProgressLabel size="40px">{counter}</CircularProgressLabel>
           </CircularProgress>
           <br /> баллов!
+          <div className="modal-information">
+            <Accordion>
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box flex="1" textAlign="left">
+                      подробнее
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4} className="modal-extended-information">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </div>
           <div className="actions">
             <ButtonGroup size="md" spacing="12">
-              {/* <Button colorScheme="green" className="green-button" onClick={handleClickClose}>
-                еще круг
-              </Button> */}
               <Link href="/">
                 <Button colorScheme="red">закрыть</Button>
               </Link>
