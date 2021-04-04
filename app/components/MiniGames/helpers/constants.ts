6fa8fbc087b5e321bd7b442c85ffba42102aebe6
Yasya Mashnenko <ya.mashnenko@gmail.com>
2021-04-04 17:26:19 +0300

    feat: add colors for theme change

--- app/components/MiniGames/helpers/constants.ts
+++ app/components/MiniGames/helpers/constants.ts
@@ -2,3 +2,38 @@ export const egg = 'https://img.icons8.com/color/96/000000/kawaii-egg.png';
 export const dinoEgg = 'https://img.icons8.com/color/96/000000/dinosaur-egg.png';
 export const dino = 'https://img.icons8.com/color/96/000000/kawaii-dinosaur--v2.png';
 export const dragon = 'https://img.icons8.com/color/96/000000/european-dragon.png';
+
+// export const pink = {
+//   LIGHT: '#F9B9B7',
+//   DARK: '#F06C9B',
+// };
+
+// export const blue = {
+//   LIGHT: '#9fd8df',
+//   DARK: '#61A0AF',
+// };
+
+export const white = {
+  LIGHT: 'whiteAlpha',
+  DARK: 'gray',
+};
+
+export const modalEntranceBackground = {
+  LIGHT: '#f5c0c0',
+  DARK: '#61A0AF',
+};
+
+export const modalBoxColor = {
+  LIGHT: '#9fd8df',
+  DARK: '#f5c0c0',
+};
+
+//   const green = {
+//     LIGHT:
+//     DARK:
+//   }
+
+//   const red = {
+//     LIGHT:
+//     DARK:
+//   }
