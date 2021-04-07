77853a7d9ef2928ad6feff8ad3bc9f9bcfc9238c
Yasya Mashnenko <ya.mashnenko@gmail.com>
2021-04-07 19:41:58 +0300

    fix: change jest method

--- app/components/MiniGames/helpers/__tests__/utils.newgame.spec.ts
+++ app/components/MiniGames/helpers/__tests__/utils.newgame.spec.ts
@@ -3,18 +3,18 @@ describe('utils, newGame', () => {
   it('should return true', () => {
     const wordToCheck = { word: 'apple', wordTranslate: 'яблоко' };
     const answer = ' AppLe  ';
-    expect(checkAnswerNewGame(wordToCheck, answer)).toBe(true);
+    expect(checkAnswerNewGame(wordToCheck, answer, false)).toBe(true);
   });
 
   it('should return false', () => {
     const wordToCheck = { word: 'apple', wordTranslate: 'яблоко' };
     const answer = 'яблоко';
-    expect(checkAnswerNewGame(wordToCheck, answer)).toBe(false);
+    expect(checkAnswerNewGame(wordToCheck, answer, false)).toBe(false);
   });
 
   it('should return false', () => {
     const wordToCheck = { word: 'apple', wordTranslate: 'яблоко' };
     const answer = 'car';
-    expect(checkAnswerNewGame(wordToCheck, answer)).toBe(false);
+    expect(checkAnswerNewGame(wordToCheck, answer, false)).toBe(false);
   });
 });
