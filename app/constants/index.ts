5871a270c9a52c94b0e5ccdd3bf18cd8ea2cd6b9
alpoliakov <alpoliakov73@gmail.com>
2021-04-16 14:10:36 +0300

    feat: Added link.

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,4 +1,5 @@
-const LOCAL_HOST = 'http://localhost:8000/';
+// const LOCAL_HOST = 'http://localhost:8000/';
+const LOCAL_HOST = 'https://rslang-team16-alpoliakov.herokuapp.com/';
 const REG_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const MenuTitle = {
